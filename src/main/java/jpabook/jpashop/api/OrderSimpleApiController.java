f93875274f37ff18cd2d8367529aa2e0ab256bfa
bomingeem <bomingeem@gmail.com>
2022-08-10 22:15:32 +0900

    API 개발 고급 - 지연 로딩과 조회 성능 최적화 복습

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -29,6 +29,11 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
     private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
+    /**
+     * V1: 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZE=null 처리
+     * - 양방향 관계 문제 발생 → @JsonIgnore
+     */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -39,6 +44,10 @@ public class OrderSimpleApiController {
         return all;
     }
 
+    /**
+     * V2: 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> ordersV2() {
         //N + 1 → 1 + 회원 N + 배송 N
@@ -48,6 +57,10 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * V3: 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     */
     @GetMapping("/api/v3/simple-orders")
     public List<SimpleOrderDto> ordersV3() {
         return orderRepository.findAllWithMemberDelivery().stream()
@@ -55,6 +68,11 @@ public class OrderSimpleApiController {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * V4: JPA에서 DTO로 바로 조회
+     * - 쿼리 1번 호출
+     * - select 절에서 원하는 데이터만 선택해서 조회
+     */
     @GetMapping("/api/v4/simple-orders")
     public List<OrderSimpleQueryDto> ordersV4() {
         return orderSimpleQueryRepository.findOrderDtos();
